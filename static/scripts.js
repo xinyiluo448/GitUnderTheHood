2a1d5f8f0bc393d148f0ef639229b7b0a54f8a9e
Ria <xinyiluo2957@gmail.com>
2025-04-18 04:40:52 -0400

    Implement quiz page 3 and 4

--- static/scripts.js
+++ static/scripts.js
@@ -1,21 +1,41 @@
 $(document).ready(function () {
 	const correctAnswer = "Blob";
 
-	$(".option-btn").on("click", function () {
-		// Reset all buttons
-		$(".option-btn")
-			.removeClass("btn-success btn-danger")
-			.addClass("btn-outline-dark");
+	if (window.location.pathname.includes('/quiz/2')) {
+		$(".option-btn").on("click", function () {
+			// Reset all buttons
+			$(".option-btn")
+				.removeClass("btn-success btn-danger")
+				.addClass("btn-outline-dark");
 
-		const selected = $(this).data("value");
+			const selected = $(this).data("value");
 
-		if (selected === correctAnswer) {
-			console.log("correct");
-			$(this).removeClass("btn-outline-dark").addClass("btn-success");
-			$("#next-btn").removeClass("d-none");
-		} else {
-			$(this).removeClass("btn-outline-dark").addClass("btn-danger");
-			$("#next-btn").addClass("d-none");
-		}
-	});
+			if (selected === correctAnswer) {
+				$(this).removeClass("btn-outline-dark").addClass("btn-success");
+				$("#next-btn").removeClass("d-none");
+			} else {
+				$(this).removeClass("btn-outline-dark").addClass("btn-danger");
+				$("#next-btn").addClass("d-none");
+			}
+		});
+	}
+
+	if (window.location.pathname.includes('/quiz/3') || window.location.pathname.includes('/quiz/4')) {
+		// Handle image click event
+		$(".clickable-image").on("click", function () {
+			var selectedIndex = $(this).data("index");
+
+			// If left image (index 1) is clicked, apply green border and show next button
+			// there might be a better way to handle this, but it works for now
+			if (selectedIndex === 1) {
+				$("[data-index=2]").removeClass("red-border");
+				$(this).addClass("green-border");
+				$("#next-btn").removeClass("d-none");
+				$(".clickable-image").off("click");
+			} else {
+				$(this).addClass("red-border"); 
+				$("#next-btn").addClass("d-none");
+			}
+		});
+	}
 });
