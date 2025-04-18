43460692d1b03852ec82d5213e86a5b6beadd381
Ria <xinyiluo2957@gmail.com>
2025-04-18 01:44:05 -0400

    Architect data and display all questions

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
+import json
 
 app = Flask(__name__)
+
+def load_questions():
+	with open("data/quiz.json", 'r', encoding='utf-8') as f:
+		return json.load(f)
+
+questions = load_questions()
 
 @app.route('/')
 def homepage():
@@ -10,9 +17,13 @@ def homepage():
 def learn():
 	return render_template('base.html')
 
-@app.route('/quiz')
-def quiz():
-	return render_template('base.html')
+@app.route('/quiz/<int:page>')
+def quiz(page):
+	total_pages = len(questions)
+	question = next((q for q in questions if q['page'] == page), None)
+	if not question:
+		return "Question not found", 404
+	return render_template('quiz.html', question=question, total_pages=total_pages)
 
 if __name__ == '__main__':
    app.run(debug = True, port=5001)
