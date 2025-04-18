530f85a9f0fda675368f79f0858ec96c43381f9a
Yixiao Li <likather@usc.edu>
2025-04-18 11:18:26 -0400

    add problem5 for quiz

--- static/scripts.js
+++ static/scripts.js
@@ -20,6 +20,25 @@ $(document).ready(function () {
 		});
 	}
 
+	if (window.location.pathname.includes('/quiz/5')) {
+		$(".option-btn").on("click", function () {
+			// Reset all buttons
+			$(".option-btn")
+				.removeClass("btn-success btn-danger")
+				.addClass("btn-outline-dark");
+
+			const selected = $(this).data("value");
+
+			if (selected === correctAnswer) {
+				$(this).removeClass("btn-outline-dark").addClass("btn-success");
+				$("#next-btn").removeClass("d-none");
+			} else {
+				$(this).removeClass("btn-outline-dark").addClass("btn-danger");
+				$("#next-btn").addClass("d-none");
+			}
+		});
+	}
+
 	if (window.location.pathname.includes('/quiz/3') || window.location.pathname.includes('/quiz/4')) {
 		// Handle image click event
 		$(".clickable-image").on("click", function () {
